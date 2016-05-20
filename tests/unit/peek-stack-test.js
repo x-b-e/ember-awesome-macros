2055724baf6989930fe186a0725fc6555b5d522a
kelly.selden <kelly.selden@classmates.com>
2016-05-20 12:13:11 -0700

    adding peekQueue and peekStack

---
+++ tests/unit/peek-stack-test.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+import peekStack from 'ember-awesome-macros/peek-stack';
+import { module, test } from 'qunit';
+
+const {
+  get,
+  A: newArray
+} = Ember;
+
+const Obj = Ember.Object.extend({
+  test: peekStack('array')
+});
+
+let obj;
+
+module('Unit | Macro | join', {
+  beforeEach() {
+    obj = Obj.create({
+      array: newArray(['test1', 'test2'])
+    });
+  }
+});
+
+test('default', function(assert) {
+  assert.expect(1);
+
+  assert.strictEqual(get(obj, 'test'), 'test2');
+});
+
+test('handles array changes', function(assert) {
+  assert.expect(1);
+
+  get(obj, 'array').removeAt(1);
+
+  assert.strictEqual(get(obj, 'test'), 'test1');
+});
