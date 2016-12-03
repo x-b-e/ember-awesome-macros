50eda0c32672951f6983ba9fa1acb5d12b2e7b7c
kellyselden <kellyselden@gmail.com>
2016-12-02 16:35:40 -0800

    consolidate normalizeArray2 tests

---
+++ tests/unit/utils/normalize-array-2-test.js
@@ -0,0 +1,58 @@
+import { normalizeArray2 } from 'ember-awesome-macros/utils';
+import { raw } from 'ember-awesome-macros';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
+import compute from '../../helpers/compute';
+
+const firstParam = 'first param test';
+const secondParam = 'second param test';
+const returnValue = 'return value test';
+
+let funcStub;
+let array;
+
+module('Unit | Macro | utils | normalize array 2', {
+  beforeEach() {
+    funcStub = sinon.stub().returns(returnValue);
+    array = { func: funcStub };
+  }
+});
+
+function computed(...keys) {
+  return normalizeArray2(keys, 'func');
+}
+
+test('it returns undefined if array undefined', function(assert) {
+  compute({
+    assert,
+    computed: computed('array'),
+    strictEqual: undefined
+  });
+});
+
+test('it calls func on array', function(assert) {
+  let { val } = compute({
+    computed: computed('array', 'firstParam', 'secondParam'),
+    properties: {
+      array,
+      firstParam,
+      secondParam
+    }
+  });
+
+  assert.deepEqual(funcStub.args, [[firstParam, secondParam]]);
+  assert.strictEqual(val, returnValue);
+});
+
+test('composable: it calls func on array', function(assert) {
+  let { val } = compute({
+    computed: computed(
+      raw(array),
+      raw(firstParam),
+      raw(secondParam)
+    )
+  });
+
+  assert.deepEqual(funcStub.args, [[firstParam, secondParam]]);
+  assert.strictEqual(val, returnValue);
+});
