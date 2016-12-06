f4737800cb9f49d98de8678d33934fd2d37506c6
kellyselden <kellyselden@gmail.com>
2016-12-06 12:04:38 -0800

    fix plugin resolution bug

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ module.exports = {
   included: function(app) {
     this._super.included.apply(this, arguments);
 
+    var plugin = require('babel-plugin-fake-import-specifiers');
+    plugin.baseDir = function() {
+      return __dirname;
+    };
+
     var babel = app.options.babel;
     babel.plugins = babel.plugins || [];
-    babel.plugins.push('babel-plugin-fake-import-specifiers');
+    babel.plugins.push(plugin);
     babel.extra = babel.extra || {};
     babel.extra['fake-import-specifiers'] = [
       'ember-awesome-macros/math'
