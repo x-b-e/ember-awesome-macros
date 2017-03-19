f816ae479ae34152d6530b66eb42023f568f9e3a
Kelly Selden <kellyselden@gmail.com>
2017-03-18 22:08:51 -0700

    polyfill uniqBy for older ember versions

--- addon/array/uniq-by.js
+++ addon/array/uniq-by.js
@@ -1,8 +1,27 @@
 import { normalizeArray } from './-utils';
+import { A as emberA } from 'ember-array/utils';
+import get from 'ember-metal/get';
+import { guidFor } from 'ember-metal/utils';
 
 export default normalizeArray({}, (array, key) => {
   if (key === undefined) {
     return array;
+  }
+  if (!array.uniqBy) {
+    // TODO: polyfill this
+    // from https://github.com/emberjs/ember.js/blob/v2.11.0/packages/ember-runtime/lib/mixins/enumerable.js#L1094-L1105
+    let ret = emberA();
+    let seen = Object.create(null);
+
+    array.forEach(item => {
+      let guid = guidFor(get(item, key));
+      if (!(guid in seen)) {
+        seen[guid] = true;
+        ret.push(item);
+      }
+    });
+
+    return ret;
   }
   return array.uniqBy(key);
 });
