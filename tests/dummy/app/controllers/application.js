e150cf18c2bbaf784816442fb11c9d70add9a7a3
Kelly Selden <kellyselden@gmail.com>
2017-04-16 12:50:43 -0700

    remove custom math test now that we have import tests

--- tests/dummy/app/controllers/application.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-import { ceil } from 'ember-awesome-macros/math';
-import floor from 'ember-awesome-macros/math/floor';
-
-export default Ember.Controller.extend({
-  ceil: ceil(0.5),
-  floor: floor(1.5)
-});
