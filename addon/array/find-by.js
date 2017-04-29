3244f41753f52ee10751101a8fcf185bb2c48933
Kelly Selden <kellyselden@gmail.com>
2017-04-29 15:27:24 -0700

    remove jshint references

--- addon/array/find-by.js
+++ addon/array/find-by.js
@@ -2,9 +2,7 @@ import createClassComputed from 'ember-macro-helpers/create-class-computed';
 import computed from 'ember-macro-helpers/computed';
 import normalizeArrayKey from 'ember-macro-helpers/normalize-array-key';
 
-/* jshint -W080 */
 const defaultValue = undefined;
-/* jshint +W080 */
 
 export default createClassComputed(
   [false, true, false],
