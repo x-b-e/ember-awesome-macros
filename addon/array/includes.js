3186e30ac8d3dfe37be4dbfa21e7dad81b498a5c
Kelly Selden <kellyselden@gmail.com>
2017-09-08 14:51:50 -0700

    use ember array's includes method (#394)

--- addon/array/includes.js
+++ addon/array/includes.js
@@ -1,5 +1,6 @@
+import { A as emberA } from '@ember/array';
 import { normalizeArray } from './-utils';
 
 export default normalizeArray({ defaultValue: () => false }, (array, item) => {
-  return array.indexOf(item) !== -1;
+  return emberA(array).includes(item);
 });
