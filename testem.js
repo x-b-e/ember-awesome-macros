f64522a96451e8048cd178010e8b26b686377674
Kelly Selden <kellyselden@gmail.com>
2018-05-04 22:40:42 -0700

    use Firefox headless

--- testem.js
+++ testem.js
@@ -6,5 +6,13 @@ module.exports = {
   ],
   launch_in_dev: [
     'Chrome'
-  ]
+  ],
+  browser_args: {
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '-headless'
+      ]
+    }
+  }
 };
