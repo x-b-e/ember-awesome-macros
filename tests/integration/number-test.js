1f551c6e4d51a53383283421add520c8859e2b5c
Kelly Selden <kellyselden@gmail.com>
2018-05-06 08:05:00 -0700

    new test syntax

--- tests/integration/number-test.js
+++ tests/integration/number-test.js
@@ -2,60 +2,60 @@ import { number } from 'ember-awesome-macros';
 import { module, test } from 'qunit';
 import compute from 'ember-macro-test-helpers/compute';
 
-module('Integration | Macro | number');
-
-test('it returns NaN if prop is undefined', function(assert) {
-  compute({
-    assert,
-    computed: number('prop'),
-    assertion(result) {
-      return isNaN(result);
-    }
-  });
-});
-
-test('it calls Number on property', function(assert) {
-  let { result } = compute({
-    computed: number('prop'),
-    properties: {
-      prop: 123.4
-    }
+module('Integration | Macro | number', function() {
+  test('it returns NaN if prop is undefined', function(assert) {
+    compute({
+      assert,
+      computed: number('prop'),
+      assertion(result) {
+        return isNaN(result);
+      }
+    });
   });
 
-  assert.strictEqual(result, 123.4);
-});
+  test('it calls Number on property', function(assert) {
+    let { result } = compute({
+      computed: number('prop'),
+      properties: {
+        prop: 123.4
+      }
+    });
 
-test('it converts boolean true to 1', function(assert) {
-  let { result } = compute({
-    computed: number('prop'),
-    properties: {
-      prop: true
-    }
+    assert.strictEqual(result, 123.4);
   });
 
-  assert.strictEqual(result, 1);
-});
+  test('it converts boolean true to 1', function(assert) {
+    let { result } = compute({
+      computed: number('prop'),
+      properties: {
+        prop: true
+      }
+    });
 
-test('it converts boolean false to 0', function(assert) {
-  let { result } = compute({
-    computed: number('prop'),
-    properties: {
-      prop: false
-    }
+    assert.strictEqual(result, 1);
   });
 
-  assert.strictEqual(result, 0);
-});
+  test('it converts boolean false to 0', function(assert) {
+    let { result } = compute({
+      computed: number('prop'),
+      properties: {
+        prop: false
+      }
+    });
 
-test('it converts strings into NaN', function(assert) {
-  compute({
-    assert,
-    computed: number('prop'),
-    properties: {
-      prop: 'foo'
-    },
-    assertion(result) {
-      return isNaN(result);
-    }
+    assert.strictEqual(result, 0);
+  });
+
+  test('it converts strings into NaN', function(assert) {
+    compute({
+      assert,
+      computed: number('prop'),
+      properties: {
+        prop: 'foo'
+      },
+      assertion(result) {
+        return isNaN(result);
+      }
+    });
   });
 });
